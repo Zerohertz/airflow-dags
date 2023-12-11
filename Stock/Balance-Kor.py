ffee86894a9d4586ec240f4d908adbdcfd8c925c
Zerohertz <ohg3417@gmail.com>
2023-12-11 09:35:26 +0900

    :sparkles: Add: Balance

---
+++ Stock/Balance-Kor.py
@@ -0,0 +1,42 @@
+import datetime as dt
+
+from airflow.decorators import dag
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+    KubernetesPodOperator,
+)
+from kubernetes.client.models import V1Volume, V1VolumeMount
+
+from lib import Environment
+
+ENV = Environment("STOCK")
+
+volume_config = V1Volume(
+    name="stock-pvc",
+    persistent_volume_claim={"claimName": "airflow-stock-pvc"},
+)
+volume_mount = V1VolumeMount(name="stock-pvc", mount_path="/app/stock", read_only=False)
+
+
+@dag(
+    dag_id="Balance-Kor",
+    start_date=dt.datetime(1998, 10, 23),
+    schedule_interval="1,31 9-15 * * 1-5",
+    max_active_runs=1,
+    catchup=False,
+    tags=["zerohertzLib", "Slack", "Stock"],
+)
+def Stock():
+    Stock = KubernetesPodOperator(
+        task_id="Stock",
+        name="Stock",
+        image="zerohertzkr/airflow-balance",
+        env_vars={
+            "SLACK": ENV.SLACK,
+            "KOR": "1",
+        },
+        volumes=[volume_config],
+        volume_mounts=[volume_mount],
+    )
+
+
+DAG = Stock()
