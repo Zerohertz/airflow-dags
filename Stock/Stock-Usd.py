9a98c7fc1b8096ffd1ccee1df2ffba8df4ebe50f
Zerohertz <ohg3417@gmail.com>
2024-02-01 09:07:37 +0900

    :sparkles: Add: Airflow USD

---
+++ Stock/Stock-Usd.py
@@ -0,0 +1,32 @@
+import datetime as dt
+
+from airflow.decorators import dag
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+    KubernetesPodOperator,
+)
+
+from lib import Environment
+
+ENV = Environment("STOCK")
+
+
+@dag(
+    dag_id="Stock-Usd",
+    start_date=dt.datetime(1998, 10, 23),
+    schedule_interval="0 13 * * *",
+    max_active_runs=1,
+    catchup=False,
+    tags=["zerohertzLib", "Slack", "Stock"],
+)
+def Stock():
+    Stock = KubernetesPodOperator(
+        task_id="Stock",
+        name="Stock",
+        image="zerohertzkr/airflow-usd",
+        env_vars={
+            "SLACK": ENV.SLACK,
+        },
+    )
+
+
+DAG = Stock()
